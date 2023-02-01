4b92ba7f90f3316e18e6bae577d290c1d72655b4
Nidia Ortiz <123881386+ngortiz@users.noreply.github.com>
2023-01-31 22:43:31 -0300

    se agrega modificaciones (#4)

--- src/App.js
+++ src/App.js
@@ -1,32 +1,40 @@
-import "./App.css";
-import Boton from "./componentes/Boton";
-import freeCodeCampLogo from "./imagenes/freecodecamp-logo.png";
+
+import './App.css';
+import Boton from './componentes/Boton';
+import Contador from './componentes/Contador';
+import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
+import { useState } from 'react';
 
 function App() {
+
+  const [numClics, setNumClics] = useState(0);
+
   const manejarClic = () => {
-    console.log("Se hace Clic");
-  };
+    setNumClics(numClics + 1);
+  }
 
   const reiniciarContador = () => {
-    console.log("Reiniciando contador");
-  };
+    setNumClics(0);
+  }
 
   return (
-    <div className="App">
-      <div className="freecodecamp-logo-contenedor">
-        <img
-          className="freecodecamp-logo"
+    <div className='App'>
+      <div className='freecodecamp-logo-contenedor'>
+        <img 
+          className='freecodecamp-logo'
           src={freeCodeCampLogo}
-          alt="Logo de freeCodeCamp"
-        />
+          alt='Logo de freeCodeCamp' />
       </div>
-      <div className="contenedor-principal">
-        <Boton texto="Clic" esBotonDeClic={true} manejarClic={manejarClic} />
-        <Boton
-          texto="Reiniciar"
+      <div className='contenedor-principal'>
+        <Contador numClics={numClics} />
+        <Boton 
+          texto='Clic'
+          esBotonDeClic={true}
+          manejarClic={manejarClic} />
+        <Boton 
+          texto='Reiniciar'
           esBotonDeClic={false}
-          manejarClic={reiniciarContador}
-        />
+          manejarClic={reiniciarContador} />
       </div>
     </div>
   );
